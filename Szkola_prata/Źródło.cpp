4d60879148c2ba64fb6a7707670a608ba0a11088
kamilizmer <pioxis939@gmail.com>
2017-10-24 00:32:13 +0200

    Funkcje cin i cout oraz biale znaki

--- Szkola_prata/Źródło.cpp
+++ Szkola_prata/Źródło.cpp
@@ -1,9 +1,25 @@
+#include <cmath>;
 #include <iostream>;
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello world" << endl;
+	int x = 0;
+	int y = 0;
+		int zesp1[2] = { 2,5 };
+		int zesp2[2] = { 6,12 };
+
+	cout << "Podaj pierwsza liczbe zespolona gdzie x = " << endl;
+	cin >> x;
+	cout << ", a iy = " << endl;
+	cin >> y;
+	zesp1[0] = x;
+	zesp1[1] = y;
+
+	cout << zesp1[0] << " + i" << zesp1[1] << endl;
+	cout << zesp2[0] << " + i" << zesp2[1] << endl;
+
 	system("pause");
 	return 0;
 
