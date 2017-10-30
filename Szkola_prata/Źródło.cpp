c8c0cf67f3f1f11444a660e1a22df19b38c902f3
kamilizmer <pioxis939@gmail.com>
2017-10-30 11:49:14 +0100

    moduly liczb zespolonych

--- Szkola_prata/Źródło.cpp
+++ Szkola_prata/Źródło.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 int main()
 {
-	int x = 0;
-	int y = 0;
-		int zesp1[2] = { 2,5 };
-		int zesp2[2] = { 6,12 };
+	int x;
+	int y;
+		int zesp1[2] = { 2, 5 };
+		int zesp2[2] = { 6, 12 };
 
 	cout << "Podaj pierwsza liczbe zespolona gdzie x = " << endl;
 	cin >> x;
@@ -19,6 +19,12 @@ int main()
 
 	cout << zesp1[0] << " + i" << zesp1[1] << endl;
 	cout << zesp2[0] << " + i" << zesp2[1] << endl;
+
+	double mod1 = sqrt(pow(zesp1[0], 2) + pow(zesp1[1], 2));
+	double mod2 = sqrt(pow(zesp2[0], 2) + pow(zesp2[1], 2));
+
+	cout << "Wynik modulu dla Z1 = " << mod1 << endl;
+	cout << "Wynik modulu dla Z2 = " << mod2 << endl;
 
 	system("pause");
 	return 0;
